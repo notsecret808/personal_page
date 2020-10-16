0d8f4c24446ac2148dc1a0fdf47151fbb4105ace
Levandro <leongtrack@gmail.com>
2020-10-16 17:58:28 +0300

    Updates

---
+++ precache-manifest.7c8cfe6a23ef7af179df14ec2727a4af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a023a93cbbbac34b82b5188aaa16cf6",
+    "url": "/personal_page/index.html"
+  },
+  {
+    "revision": "97bada0f4fd4cd183f74",
+    "url": "/personal_page/static/css/main.67bd8808.chunk.css"
+  },
+  {
+    "revision": "d7c22012e3ce8c57ec46",
+    "url": "/personal_page/static/js/2.d1913e96.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/personal_page/static/js/2.d1913e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97bada0f4fd4cd183f74",
+    "url": "/personal_page/static/js/main.4bd47a21.chunk.js"
+  },
+  {
+    "revision": "1fcd1bbcfe373fc397d3",
+    "url": "/personal_page/static/js/runtime-main.fbc7c06a.js"
+  }
+]);
