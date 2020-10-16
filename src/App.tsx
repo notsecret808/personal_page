708e3922c41796ad66773a177b5c1808c2f3346e
Levandro <leongtrack@gmail.com>
2020-10-16 14:10:22 +0300

    feauture: basic design created

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './css/AppStyle.css';
+
+type LinkInfo = {
+    ServiceName: string,
+    Link?: string,
+    BackgroundStyle: string,
+    setBackgroundStyle: (style:string) => void,
+}
+
+function Link(props: LinkInfo) {
+    return (
+        <div onClick={() => props.setBackgroundStyle(props.BackgroundStyle)} className={'link-wrapper'}>
+            <div className={'link-container'}>
+                <p>{props.ServiceName}</p>
+            </div>
+        </div>
+    )
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [background, setBackground] = useState('main-wrapper');
+    return (
+        <div id={background} className = {'main'}>
+            <div id={'layout-opacity'}>
+                <div id={'menu-wrapper'}>
+                    <div id={'menu'}>
+                        <Link setBackgroundStyle={setBackground} BackgroundStyle={'vk-wrapper'} ServiceName={'VK'}/>
+                        <Link setBackgroundStyle={setBackground} BackgroundStyle={'tg-wrapper'} ServiceName={'TG'}/>
+                        <Link setBackgroundStyle={setBackground} BackgroundStyle={'github-wrapper'} ServiceName={'GITHUB'}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
